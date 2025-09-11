8d362e4e2ee22771dc1b6dc4e4f8182df39a9062
aminulaiba <aminul.islam.f.edu.p@gmail.com>
2025-09-11 16:26:14 +0600

    4th commit: has been extended user profiles(more info about user), signal to auto create the profile when a user get regestered, and password change with no old password(later need to do with old pass)

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# class UserAdmin(admin.ModelAdmin):
+#     model = User
+#     fields = []
+
+class UserAdmin(DefaultUserAdmin):
+    inlines = [ProfileInline,]
+    
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+
+    path('update-user/', views.update_user, name='update-user'),
+    path('update-profile/', views.update_profile, name='update-profile'),
+    path('pass-change/', views.password_change, name='password-change'),
     path('detail/<int:pk>', views.product, name='product'),
     path('category/<str:name>', views.category, name='category'),
 ] 
