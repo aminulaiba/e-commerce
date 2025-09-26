1078672bcac8b78c4dfbd2bb696a965cc374fd63
aminulaiba <aminul.islam.f.edu.p@gmail.com>
2025-09-26 19:13:46 +0600

    Refactor cart handling: move session cart to DB on login, add CartManager for dynamic storage

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,12 @@
 from store.models import Product
-class Cart:
+from .models import Cart, CartItem
+
+from django.db.models import Sum
+from django.shortcuts import get_object_or_404
+
+
+#  cart from session for annonymous user
+class SessionCart:
     def __init__(self, request):
         self.session = request.session
         cart = self.session.get('cart')
@@ -14,16 +21,6 @@ class Cart:
         # Optional: explicitly mark the session as modified
         self.session.modified = True
 
-    # def count_items(self):
-    #     return sum(item['qty'] for item in self.cart.values())
-
-
-
-    # Calculating the subprice of an item
-    def sub_price(self):
-        pass
-
-
     def add(self, product_id, quantity=1):
         product_id = str(product_id)
         if product_id in self.cart:
@@ -31,9 +28,7 @@ class Cart:
         else:
             self.cart[product_id] = quantity
         self.save()
-        total, cart_prods = self.checkout_totals()
-        
-        return sum(self.cart.values()), total
+        return sum(self.cart.values())
     
     def update(self, prodId, updval):
         if prodId in self.cart:
@@ -42,7 +37,6 @@ class Cart:
             total, cart_prods = self.checkout_totals()
             print('total: ', total, 'pros: ', cart_prods)
             return sum(self.cart.values()), total
-
 
     def remove(self, product_id):
         if product_id in self.cart:
@@ -55,11 +49,10 @@ class Cart:
         cart_products = []
         total, products = self.checkout_totals()
         for p_id, v in products.items():
-            dic = {'product': v, 'qty': self.cart[str(p_id)]}
+            dic = {'product': v, 'price_at_time': v.sale_price if v.on_sale else v.price, 'qty': self.cart[str(p_id)]}
             cart_products.append(dic)
         return cart_products, total
     
-
     # Final total from DB for checkout
     def checkout_totals(self):
         cart = self.cart
@@ -74,3 +67,79 @@ class Cart:
         return total, products
 
 
+
+
+
+
+#  cart from db for logged in user
+class DBCart:
+    def __init__(self, user):
+        self.user = user
+        self.cart = Cart.objects.filter(user=user, is_active=True).last()
+
+    def cart_products(self):
+        # cart_products = CartItem.objects.filter(cart=self.cart)
+        cart_products = []
+        # item structure is {'product': v, 'qty': self.cart[str(p_id)]}
+        products = self.cart.items.all()
+        for item in products:
+            dic = {'product': item.product, 'price_at_time': item.price_at_time, 'qty': item.quantity}
+            cart_products.append(dic)
+        total = self.checkout_totals()
+        return cart_products, total
+    
+    def checkout_totals(self):
+        return sum([p.price_at_time*p.quantity for p in self.cart.items.all()])
+
+    def total_quantity(self):
+        return self.cart.items.aggregate(total=Sum('quantity'))['total'] or 0
+    def item_current_price(self, product_id):
+        product = get_object_or_404(Product, id=product_id)
+        return product.price if not product.on_sale else product.sale_price
+
+    def add(self, product_id, quantity=1):
+        item, created = CartItem.objects.get_or_create(
+            cart=self.cart,
+             product_id=product_id,
+              defaults={
+                  'quantity': 1,
+                  'price_at_time': self.item_current_price(product_id)
+                  }
+            )
+        if not created:
+            item.quantity = item.quantity+ quantity
+            item.save()
+
+        total_quantity = self.cart.items.aggregate(total=Sum('quantity'))['total'] or 0
+        return total_quantity
+    
+    def update(self, prodId, updval):
+        print("hiii")
+        cart_item = self.cart.items.get(cart=self.cart, product__id=prodId)
+        cart_item.quantity = updval
+        cart_item.save()
+        total_quantity = self.total_quantity()
+        total = self.checkout_totals()
+        print(cart_item)
+        return total_quantity, total
+
+    def remove(self, product_id):
+        item = self.cart.items.get(cart=self.cart, product_id=product_id)
+        item.delete()
+        total_quantity = self.total_quantity()
+        total = self.checkout_totals()
+        return total_quantity, total
+
+
+
+#  cart manager that switches between sessio and db
+class CartManager:
+    def __init__(self, request):
+        self.request = request
+        if request.user.is_authenticated:
+            self.cart = DBCart(request.user)
+        else:
+            self.cart = SessionCart(request)
+
+    def __getattr__(self, name):
+        return getattr(self.cart, name)
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-from .cart import Cart
+from .cart import SessionCart, DBCart, CartManager
 from store.models import Product
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
@@ -9,40 +9,39 @@ from django.urls import reverse
 # Create your views here.
 
 def cart(request):
-    print(dict(request.session))
-    cart = Cart(request)
-    cart_products, subtotal_price = cart.cart_products()
-    tot=cart.checkout_totals()
-    print("actual totals from db: ", tot)
-    shipping = 30
+    cart = CartManager(request)
+    cart_products, subtotal_price =cart.cart_products()
+    shipping = 30 if subtotal_price!=0 else 0
     total = subtotal_price+shipping
-    return render(request, 'cart/cart.html', {'cart_products': cart_products, 'subtotal': subtotal_price, 'shipping': shipping, 'total': total })
+    # print('cart products: ', cart_products)
+    return render(request, 'cart/cart.html', {'cart_products': cart_products, 'subtotal': subtotal_price, 'shipping': shipping, 'total': total})
+    
 
 def cart_add(request):
-    cart = Cart(request)
+    cart = CartManager(request)
     if request.method == 'POST':
         product_id = int(json.loads(request.body).get('product_id'))
         product_qty = int(json.loads(request.body).get('qty'))
 
-        product = get_object_or_404(Product, pk=product_id)
-        total_cart_items, total = cart.add(product_id, product_qty)
-        return JsonResponse({'cart': total_cart_items, 'total': total})
+        total_cart_items = cart.add(product_id, product_qty)
+        return JsonResponse({'cart': total_cart_items})
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def cart_delete(request):
+    cart = CartManager(request)
     body = request.body
     prodId = json.loads(body).get('prodId')
-    cart = Cart(request)
+    # cart = SessionCart(request)
     total_cart_items, total = cart.remove(prodId)
     return JsonResponse({'cart': total_cart_items, 'total': total})
     # return JsonResponse({'redirect_url': reverse('cart')})
 
+# for updating the quantity in cart page
 def cart_update(request):
-    cart = Cart(request)
+    cart = CartManager(request)
     body = json.loads(request.body)
     prodId = body.get('prodId')
     updval = int(body.get('updval'))
-    # cart.update(prodId, updval)
     total_cart_items, total = cart.update(prodId, updval)
     return JsonResponse({'cart': total_cart_items, 'total': total})
 
