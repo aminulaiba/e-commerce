7b804edac5ad8c95b19cb42c1b9b676a4ab96d0e
aminulaiba <aminul.islam.f.edu.p@gamil.com>
2025-10-05 10:30:36 +0600

    managed cart page and place order page

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from cart.cart import CartManager
 
 # Create your views here.
+def order_details(request):
+    cart = CartManager(request)
+    cart_items, subtotal_price =cart.cart_products()
+
+    shipping = 30 if subtotal_price!=0 else 0
+    total = subtotal_price+shipping
+    return render(request, 'payments/checkout.html', {'cart_products': cart_items, 'subtotal': subtotal_price, 'shipping': shipping, 'total': total})
+    
